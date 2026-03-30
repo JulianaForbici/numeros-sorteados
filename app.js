09ee2af13d927d7f824f2e272800a99329870ba7
JulianaForbici <julianaforbicii@gmail.com>
2026-03-29 23:18:50 -0300

    feat: add tentativas com incremento

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-alert("Bem-vindo ao sorteio de números! Clique no botão para sortear um número entre 1 e 100.");
+alert(
+  "Bem-vindo ao sorteio de números! Clique no botão para sortear um número entre 1 e 100.",
+);
 
-let numeroSecreto = 90; 
-let chute = prompt("Digite um número entre 1 e 100 para tentar adivinhar o número secreto: ");
+let numeroSecreto = 90;
+console.log(`O número secreto é: ${numeroSecreto}`);
+let chute;
+let tentativas = 1;
 
-if (numeroSecreto == chute) {
-    alert("Parabéns! Você acertou o número secreto!");
-} else {
+while (chute != numeroSecreto) {
+  chute = prompt(
+    "Digite um número entre 1 e 100 para tentar adivinhar o número secreto: ",
+  );
+
+  if (numeroSecreto == chute) {
+    alert(`Parabéns! Você acertou o número secreto em ${tentativas} tentativas!`);
+  } else {
     if (chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`);
+      alert(`O número secreto é menor que ${chute}`);
     } else {
-        alert(`O número secreto é maior que ${chute}`);
+      alert(`O número secreto é maior que ${chute}`);
     }
+    tentativas++;
+  }
 }
