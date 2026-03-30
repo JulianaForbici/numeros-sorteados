60e3778f7b4950170e3532d02fa498a69f7d56c9
JulianaForbici <julianaforbicii@gmail.com>
2026-03-29 23:33:51 -0300

    feat: add var de numeroMax

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-alert(
-  "Bem-vindo ao sorteio de números! Clique no botão para sortear um número entre 1 e 100.",
-);
-
-let numeroSecreto = parseInt(Math.random() * 100) + 1;
+alert(`Bem-vindo ao sorteio de números!`);
+let numeroMax = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMax) + 1;
 console.log(`O número secreto é: ${numeroSecreto}`);
 let chute;
 let tentativas = 1;
 
 while (chute != numeroSecreto) {
   chute = prompt(
-    "Digite um número entre 1 e 100 para tentar adivinhar o número secreto: ",
+    `Digite um número entre 1 e ${numeroMax} para tentar adivinhar o número secreto: `,
   );
 
   if (numeroSecreto == chute) {
