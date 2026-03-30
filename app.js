411b96a9197eed3fc8b03764219f61e40fe3ff66
juliana.forbici <juliana.forbici@senior.com.br>
2026-03-30 07:56:04 -0300

    feat: add queryselector para o input

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let numeroSorteado = gerarNumeroAleatorio();
+
 function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
@@ -7,5 +9,11 @@ exibirTextoNaTela('h1', 'Sorteador de Números');
 exibirTextoNaTela('p', 'Clique no botão para sortear um número entre 1 e 100.');
 
 function verificarChute() {
-    console.log("Botão clicado!");
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSorteado);
+    console.log(numeroSorteado);
+}
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10) + 1;
 }
