aa1e234522c4ccc88ef0001f1ac24462a5278025
JulianaForbici <julianaforbicii@gmail.com>
2026-06-09 20:03:51 -0300

    feat: add merge

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-<<<<<<< HEAD
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
-=======
-let numeroSorteado = gerarNumeroAleatorio();
-let tentativas = 1;
 
->>>>>>> 0c86396fe14e86962bac8e940d067019bc6e9ccc
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
+
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
+
 exibirMensagemInicial();
+
 function verificarChute() {
     let chute = document.querySelector('input').value;
-<<<<<<< HEAD
+    
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
@@ -34,13 +32,16 @@ function verificarChute() {
         limparCampo();
     }
 }
+
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 3 + 1);
 }
+
 function limparCampo() {
     chute = document.querySelector('input');
     chute.value = '';
 }
+
 function reiniciarJogo() {
     numeroSecreto = gerarNumeroAleatorio();
     limparCampo();
@@ -48,29 +49,3 @@ function reiniciarJogo() {
     exibirMensagemInicial();
     document.getElementById('reiniciar').setAttribute('disabled', true)
 }
-=======
-    if(chute == numeroSorteado) {
-        exibirTextoNaTela('h1', 'Parabéns!');
-        let palavraTentiva = tentativas > 1 ? 'tentativas' : 'tentativa';
-        mensagemTentiva = `O número sorteado foi ${numeroSorteado} e você acertou em ${tentativas} ${palavraTentiva}.`;
-        exibirTextoNaTela('p', mensagemTentiva);
-    } else {
-        if (chute < numeroSorteado) {
-            exibirTextoNaTela('p', 'O número sorteado é maior do que o seu chute.');
-        } else {
-          exibirTextoNaTela('p', 'O número sorteado é menor do que o seu chute.');
-        }
-        tentativas++;
-        limparCampoDeChute();
-    }
-}
-
-function gerarNumeroAleatorio() {
-  return parseInt(Math.random() * 10) + 1;
-}
-
-function limparCampoDeChute() {
-  let chute = document.querySelector('input');
-  chute.value = '';
-}
->>>>>>> 0c86396fe14e86962bac8e940d067019bc6e9ccc
